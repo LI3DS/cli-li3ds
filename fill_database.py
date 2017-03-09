c1ce3c535162e98ee5af4dd1c6f26283bc8900da
arnaud <arnaud@lnv1210p036-debian.ign.fr>
2017-03-09 10:58:44 +0100

    first draft

--- fill_database.py
+++ fill_database.py
@@ -4,6 +4,26 @@ import sys
 import os
 import getopt
 
+import requests
+import json
+
+def get_earth():
+	r = requests.get('http://api.li3ds.io/sensors/')
+	data = json.loads(r.text)	
+
+
+	for i,sensor in enumerate(data):
+		for j, attr in enumerate(sensor):
+			if(attr=='short_name'):
+				name = sensor['short_name']
+				if(name=="Earth"):
+					return True
+
+	return False
+
+def post_earth():
+	print "post_earth"
+
 def read_token(src="token.txt"):
 	src = open(src, 'r')
 	lines = src.readlines()
@@ -17,4 +37,11 @@ if __name__ == '__main__':
 
 	token = read_token()
 
-	print token
+	inDatabase = get_earth()
+
+	if(inDatabase==False):
+		post_earth()
+
+	#print token
+
+	
