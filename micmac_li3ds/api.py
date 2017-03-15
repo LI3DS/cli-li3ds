b9f42c7c1c62e90efaaff85d2d4001447d745111
Éric Lemoine <eric.lemoine@oslandia.com>
2017-03-15 14:49:25 +0100

    Rework api interface and import_blinis command

--- micmac_li3ds/api.py
+++ micmac_li3ds/api.py
@@ -1,40 +1,90 @@
 import requests
 
 
-def create_referential(ref_name, sensor_id, api_url, api_key):
-    """
-    Create a referential in the li3ds database.
-    """
-    referential = {
-        'description': '',
-        'name': ref_name,
-        'root': True,
-        'sensor': sensor_id,
-        'srid': 0,
-    }
-    referentials_url = api_url.rstrip('/') + '/referentials/'
+def _create_object(obj, url, api_key):
     headers = {'X-API-KEY': api_key}
-    resp = requests.post(
-        referentials_url, json=referential, headers=headers)
-    if resp.status_code != 201:
-        err = 'Adding referential failed (status code: {})'.format(
-              resp.status_code)
-        raise RuntimeError(err)
-    return resp
+    resp = requests.post(url, json=obj, headers=headers)
+    if resp.status_code == 201:
+        obj = resp.json()
+        return obj[0]
+    err = 'Adding object failed (status code: {})'.format(
+          resp.status_code)
+    raise RuntimeError(err)
+
+
+def _get_object_by_id(obj_id, url, api_key):
+    url = url + '/{:d}/'.format(obj_id)
+    headers = {'X-API-KEY': api_key, 'Accept': 'application/json'}
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        obj = resp.json()
+        return obj[0]
+    if resp.status_code == 404:
+        return None
+    err = 'Getting object failed (status code: {})'.format(
+          resp.status_code)
+    raise RuntimeError(err)
+
+
+def get_referential(referential_id, api_url, api_key):
+    """
+    Get a referential by its id.
+    """
+    url = api_url.rstrip('/') + '/referentials'
+    return _get_object_by_id(referential_id, url, api_key)
+
+
+def create_referential(referential, api_url, api_key):
+    """
+    Create a referential.
+    """
+    url = api_url.rstrip('/') + '/referentials/'
+    return _create_object(referential, url, api_key)
 
 
 def get_sensor(sensor_id, api_url, api_key):
     """
-    Get the sensor whose id is sensor_id from the li3ds database.
+    Get a sensor by its id.
     """
-    sensor_url = api_url.rstrip('/') + '/sensors/{:d}/'.format(sensor_id)
-    headers = {'X-API-KEY': api_key, 'Accept': 'application/json'}
-    resp = requests.get(sensor_url, headers=headers)
-    if resp.status_code == 200:
-        sensors = resp.json()
-        return sensors[0]
-    if resp.status_code == 404:
-        return None
-    err = 'Getting sensor failed (status code: {})'.format(
-          resp.status_code)
-    raise RuntimeError(err)
+    url = api_url.rstrip('/') + '/sensors'
+    return _get_object_by_id(sensor_id, url, api_key)
+
+
+def create_sensor(sensor, api_url, api_key):
+    """
+    Create a sensor.
+    """
+    url = api_url.rstrip('/') + '/sensors/'
+    return _create_object(sensor, url, api_key)
+
+
+def get_transfo(transfo_id, api_url, api_key):
+    """
+    Get a transfo by its id.
+    """
+    url = api_url.rstrip('/') + '/transfos'
+    return _get_object_by_id(transfo_id, url, api_key)
+
+
+def create_transfo(transfo, api_url, api_key):
+    """
+    Create a transfo.
+    """
+    url = api_url.rstrip('/') + '/transfos/'
+    return _create_object(transfo, url, api_key)
+
+
+def get_transfotree(transfotree_id, api_url, api_key):
+    """
+    Get a transfotree by its id.
+    """
+    url = api_url.rstrip('/') + '/transfotrees'
+    return _get_object_by_id(transfotree_id, url, api_key)
+
+
+def create_transfotree(transfotree, api_url, api_key):
+    """
+    Create a transfotree.
+    """
+    url = api_url.rstrip('/') + '/transfotrees/'
+    return _create_object(transfotree, url, api_key)
--- micmac_li3ds/import_blinis.py
+++ micmac_li3ds/import_blinis.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import xml.etree.ElementTree
 
@@ -7,7 +8,11 @@ from . import api
 
 
 class ImportBlinis(Command):
-    """ Import a blinis file """
+    """ Import a blinis file
+
+        Create a sensor group if no sensor_id is provided. And create new
+        referentials and transforms for the sensor group.
+    """
 
     log = logging.getLogger(__name__)
 
@@ -24,7 +29,6 @@ class ImportBlinis(Command):
             help='the li3ds API key')
         parser.add_argument(
             '--sensor-id', '-s',
-            required=True,
             type=int,
             help='the sensor group id')
         parser.add_argument(
@@ -38,31 +42,109 @@ class ImportBlinis(Command):
         api_url = parsed_args.api_url
         api_key = parsed_args.api_key
 
-        sensor = api.get_sensor(sensor_id, api_url, api_key)
-        assert(sensor is not None)
-
         root = self.parse_blinis(parsed_args.blinis_file)
-        assert(root.tag == 'StructBlockCam')
+        if root.tag != 'StructBlockCam':
+            err = 'Parsing blinis file failed: root is not StructBlockCam'
+            raise RuntimeError(err)
 
         key_im2_time_cam_node = root.find('KeyIm2TimeCam')
-        assert(key_im2_time_cam_node is not None)
+        if key_im2_time_cam_node is None:
+            err = 'Parsing blinis file failed: no tag KeyIm2TimeCam'
+            raise RuntimeError(err)
+
+        # create a sensor group if sensor_id is not specified on the
+        # command line arguments
+        if sensor_id is None:
+            sensor = {
+                'brand': '',
+                'description': '',
+                'model': key_im2_time_cam_node.text,
+                'serial_number': '',
+                'short_name': '',
+                'specifications': {},
+                'type': 'group',
+            }
+            sensor = api.create_sensor(sensor, api_url, api_key)
+            sensor_id = sensor['id']
+            self.log.info('Sensor {:d} created.'.format(sensor_id))
+        else:
+            sensor = api.get_sensor(sensor_id, api_url, api_key)
+            if not sensor:
+                err = 'Sensor id {:d} not in db'.format(sensor_id)
+                raise RuntimeError(err)
+            if sensor['type'] != 'group':
+                err = 'Sensor id {:d} is not of type "group"'.format(sensor_id)
 
         # create the sensor group referential
-        resp = api.create_referential(
-                key_im2_time_cam_node.text, sensor_id, api_url, api_key)
-        self.log.info('Success!')
-        self.log.info(resp.text)
+        # FIXME description?
+        base_referential = {
+            'description': '',
+            'name': key_im2_time_cam_node.text,
+            'root': True,
+            'sensor': sensor_id,
+            'srid': 0,
+        }
+        base_referential = api.create_referential(
+                base_referential, api_url, api_key)
+        base_referential_id = base_referential['id']
+        self.log.info('Referential {:d} created.'.format(base_referential_id))
 
         liaisons_shc_node = root.find('LiaisonsSHC')
-        assert(liaisons_shc_node is not None)
+        if key_im2_time_cam_node is None:
+            err = 'Parsing blinis file failed: no tag LiaisonsSHC'
+            raise RuntimeError(err)
 
-        for param_orient_shc_node in liaisons_shc_node:
-            assert(param_orient_shc_node.tag == 'ParamOrientSHC')
+        param_orient_shc_nodes = liaisons_shc_node.findall('ParamOrientSHC')
 
+        transfo_ids = []
+        for param_orient_shc_node in param_orient_shc_nodes:
             id_grp_node = param_orient_shc_node.find('IdGrp')
-            sub_sensor_id = int(id_grp_node.text)
-            sub_sensor = api.get_sensor(sub_sensor_id, api_url, api_key)
-            assert(sub_sensor is not None)
+
+            # FIXME description?
+            referential = {
+                'description': '',
+                'name': id_grp_node.text,
+                'root': True,
+                'sensor': sensor_id,
+                'srid': 0,
+            }
+            referential = api.create_referential(referential, api_url, api_key)
+            referential_id = referential['id']
+            self.log.info('Referential {:d} created.'.format(referential_id))
+
+            # FIXME
+            # validity_start, validity_end and transfo_type currently
+            # hard-coded
+            transfo = {
+                'description': '',
+                'parameters': {},
+                'source': base_referential_id,
+                'target': referential_id,
+                'tdate': datetime.datetime.now().isoformat(),
+                'validity_start': '0001-01-01T00:00:00+00',
+                'validity_end': '9999-12-31T23:59:59+01',
+                'transfo_type': 2,
+            }
+            transfo = api.create_transfo(transfo, api_url, api_key)
+            transfo_id = transfo['id']
+            self.log.info('Transfo {:d} created.'.format(transfo_id))
+
+            transfo_ids.append(transfo_id)
+
+        if len(transfo_ids):
+            # FIXME owner?
+            transfotree = {
+                'isdefault': True,
+                'name': key_im2_time_cam_node.text,
+                'owner': '',
+                'sensor_connections': False,
+                'transfos': transfo_ids,
+            }
+            transfotree = api.create_transfotree(transfotree, api_url, api_key)
+            transfotree_id = transfotree['id']
+            self.log.info('Transfo tree {:d} created.'.format(transfotree_id))
+
+        self.log.info('Success!')
 
     @staticmethod
     def parse_blinis(blinis_file):
