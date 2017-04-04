d60d0d89427c4ea7c5562a8794eea4fcb5f00615
Éric Lemoine <eric.lemoine@oslandia.com>
2017-04-04 15:13:24 +0200

    Add function "children" to "util" module

--- micmac_li3ds/util.py
+++ micmac_li3ds/util.py
@@ -1,6 +1,14 @@
 def child(parent, name):
     child_node = parent.find(name)
     if child_node is None:
-        err = 'Error: no {} tag in autocal file'.format(name)
+        err = 'Error: no tag "{}" in XML'.format(name)
         raise RuntimeError(err)
     return child_node
+
+
+def children(parent, name):
+    child_nodes = parent.findall(name)
+    if not child_nodes:
+        err = 'Error: no tag "{}" in XML'.format(name)
+        raise RuntimeError(err)
+    return child_nodes
